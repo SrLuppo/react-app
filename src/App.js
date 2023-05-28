58ed270a867e0e4fc4cba714fade7709e8fe6a3b
adelano <adelano@softwar.com.br>
2023-05-28 12:14:30 -0300

    firt page text test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Word! Adelano Bora Codar!!!!
+          Hello! Bora Codar!!!!
         </p>
+        <p>A felicidade de uma pessoa pode ser influenciada por muitos fatores, e cada pessoa é única em relação ao que a faz feliz. No entanto, existem algumas dicas gerais que podem ajudar a melhorar o relacionamento e contribuir para a felicidade de sua esposa. Aqui estão algumas sugestões:</p>
+
+        <p>Comunicação: A comunicação é fundamental em qualquer relacionamento saudável. Ouça atentamente sua esposa, mostre interesse genuíno em suas opiniões, sentimentos e experiências. Compartilhe também seus próprios pensamentos e sentimentos.</p>
+
+        <p>Apoio emocional: Esteja presente para apoiar sua esposa em momentos difíceis. Mostre empatia, compreensão e ofereça conforto quando ela precisar. Demonstre que você está lá para ela.</p>
+
+        <p>Gestos de carinho: Pequenos gestos de carinho e afeto podem ter um grande impacto. Surpreenda sua esposa com um abraço, beijo ou elogio inesperado. Demonstre seu amor e apreço por ela.</p>
+
+        <p>Compartilhar responsabilidades: Dividir as tarefas domésticas e as responsabilidades do dia a dia pode ajudar a reduzir o estresse e a sobrecarga de sua esposa. Contribua ativamente em casa e assuma sua parte justa das responsabilidades.</p>
+
+        <p>Tempo de qualidade juntos: Reserve momentos especiais para passar com sua esposa. Planeje encontros românticos, atividades divertidas ou simplesmente um tempo tranquilo para conversar e se conectar.</p>
+
+        <p>Mostre interesse por seus interesses: Demonstre interesse genuíno pelas coisas que sua esposa gosta e se envolva em suas atividades. Isso mostra que você valoriza os interesses dela e está disposto a compartilhar experiências.</p>
+
+        <p>Surpreenda-a: De vez em quando, surpreenda sua esposa com algo especial. Pode ser um presente, uma viagem ou até mesmo uma carta de amor. Mostre que você pensa nela e se esforça para tornar sua vida mais feliz.</p>
+
+        <p>Respeito mútuo: Respeite os desejos, opiniões e limites de sua esposa. Trate-a com respeito em todas as situações e evite críticas ou julgamentos desnecessários.</p>
+
 
       </header>
     </div>
